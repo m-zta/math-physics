fa1f4b114f0c69a5287ab64f8a99a0120da5b445
Mario Arana <marioknaus22@gmail.com>
2023-07-28 22:52:41 +0200

    Minor changes

--- dirac_function.py
+++ dirac_function.py
@@ -1,29 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def L(x, n):
-    if n > 0:
-        return 1 / np.pi * n / ((x)**2 + n**2)
-    elif n <= 0:
-        return 0
+# Different representations of the Dirac delta function
 
-def g(x, a):
-    return np.where(np.abs(x) <= a, 1/(2*a), 0)
+
+def L(x, h):
+    """Lorentzian curve with half-width h"""
+    return np.where(h > 0, 1 / np.pi * h / ((x)**2 + h**2), 0)
+
+
+def G(x, h):
+    """Gaussian curve with standard deviation h"""
+    return np.where(np.abs(x) <= h, 1/(2*h), 0)
+
 
 def main():
     x = np.linspace(-21, 21, 1000)
 
-    for n in np.linspace(2, 0.1, 200):
-        a = 3 # x-value of the peak of the dirac delta function
+    for n in np.linspace(1, 0.05, 100):
+        a = 3  # x-value of the peak of the dirac delta function
         y = L(x - a, n)
-        plt.plot(x, y)
+        plt.plot(x, y, 'r-')
         plt.ylim(-0.1, 10.1)
+        plt.grid(True)
         plt.title(f'Graph of the function L(x - {a}, {n:.2f})')
         plt.draw()
         plt.pause(0.01)
         plt.clf()
 
-    plt.show() # Keeps the plot open until the user closes it
+    plt.show()  # Keeps the plot open until the user closes it
+
 
 if __name__ == '__main__':
     main()
